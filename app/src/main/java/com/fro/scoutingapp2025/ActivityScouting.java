dab856cd53a2dd26bd7d4db65b7f3a076e3c8466
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-02-13 23:38:05 -0500

    Created fragment_template, and created component top text

--- app/src/main/java/com/fro/scoutingapp2025/ActivityScouting.java
+++ app/src/main/java/com/fro/scoutingapp2025/ActivityScouting.java
@@ -3,6 +3,9 @@ package com.fro.scoutingapp2025;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ActivityScouting extends AppCompatActivity {
     // Declare global variables here \/
@@ -15,5 +18,14 @@ public class ActivityScouting extends AppCompatActivity {
 
         // Instantiate variables here \/
 
+
+        replaceFragment(new FragmentTemplate());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.replaceFrame, fragment);
+        fragmentTransaction.commit();
     }
 }
