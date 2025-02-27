8626dbd14a7c3ee1830d893282c4dc8c600e45f1
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-02-27 13:06:12 -0500

    help screen on scouting page only

--- app/src/main/java/com/fro/scoutingapp2025/ActivityScouting.java
+++ app/src/main/java/com/fro/scoutingapp2025/ActivityScouting.java
@@ -1,12 +1,14 @@
 package com.fro.scoutingapp2025;
 
 import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,6 +36,16 @@ public class ActivityScouting extends AppCompatActivity {
     LinearLayout confirmPopup;
     TextView confirmYes;
     TextView confirmNo;
+    LinearLayout helpPopup;
+    TextView helpHeader1;
+    TextView helpHeader2;
+    TextView helpText1;
+    TextView helpText2;
+    ImageView helpImage;
+    ImageButton helpLeftArrow;
+    ImageButton helpRightArrow;
+    TextView helpPageNumber;
+    ImageButton helpExitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +69,7 @@ public class ActivityScouting extends AppCompatActivity {
         confirmPopup = findViewById(R.id.confirm_popup);
         confirmYes = findViewById(R.id.confirm_yes);
         confirmNo = findViewById(R.id.confirm_no);
+        helpPopup = findViewById(R.id.help_popup);
 
         // Inflate all fragments so it creates all data in the list
         replaceFragment(new FragmentTeleop());
@@ -71,7 +84,7 @@ public class ActivityScouting extends AppCompatActivity {
         notesButton.setOnClickListener(v -> replaceFragment(new FragmentNotes()));
 
 
-        // Set the size and position of menu popup and submit popup
+        // Set the size and position of popups
         fullPage.post(new Runnable() {
             @Override
             public void run() {
@@ -88,6 +101,13 @@ public class ActivityScouting extends AppCompatActivity {
                 confirmParams.width = (int) (fullPage.getWidth()/1.8);
                 confirmParams.height = fullPage.getHeight()/3;
                 confirmPopup.setLayoutParams(confirmParams);
+
+                // Help popup
+                ViewGroup.MarginLayoutParams helpParams = (ViewGroup.MarginLayoutParams) helpPopup.getLayoutParams();
+                helpParams.width = (int) (fullPage.getWidth()/1.1);
+                helpParams.height = (int) (fullPage.getHeight()/1.1) - bottomBar.getHeight();
+                helpParams.bottomMargin = bottomBar.getHeight() + fullPage.getHeight()/100;
+                helpPopup.setLayoutParams(helpParams);
             }
         });
 
@@ -109,22 +129,25 @@ public class ActivityScouting extends AppCompatActivity {
             menuExit.setVisibility(GONE);
             menuPopup.setVisibility(GONE);
             confirmPopup.setVisibility(GONE);
+            helpPopup.setVisibility(GONE);
+            helpButton.setClickable(true);
+            helpButton.setImageResource(R.drawable.help_button);
             fullPage.setClickable(false);
         });
-        homeButton.setOnClickListener(v ->{
+        homeButton.setOnClickListener(v -> {
             menuBars.setVisibility(VISIBLE);
             menuExit.setVisibility(GONE);
             menuPopup.setVisibility(GONE);
             fullPage.setClickable(false);
             startActivity(new Intent(getApplicationContext(), ActivityMain.class));
         });
-        submitButton.setOnClickListener(v ->{
+        submitButton.setOnClickListener(v -> {
             confirmPopup.setVisibility(VISIBLE);
             fullPage.setClickable(false);
             confirmText.setText(Values.checkData());
         });
 
-        confirmYes.setOnClickListener( v ->{
+        confirmYes.setOnClickListener(v -> {
             menuBars.setVisibility(VISIBLE);
             menuExit.setVisibility(GONE);
             menuPopup.setVisibility(GONE);
@@ -135,9 +158,80 @@ public class ActivityScouting extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), ActivityMain.class));
         });
 
-        confirmNo.setOnClickListener( v ->{
+        confirmNo.setOnClickListener(v -> {
             confirmPopup.setVisibility(GONE);
             fullPage.setClickable(true);
+        });
+
+        // Help page
+        helpButton.setOnClickListener(v -> {
+            helpPopup.setVisibility(VISIBLE);
+            fullPage.setClickable(true);
+            helpButton.setImageResource(R.drawable.menu_exit);
+            helpButton.setClickable(false);
+
+            //init pages
+            helpHeader1 = findViewById(R.id.help_header_1);
+            helpText1 = findViewById(R.id.help_text_1);
+            helpHeader2 = findViewById(R.id.help_header_2);
+            helpText2 = findViewById(R.id.help_text_2);
+            helpImage = findViewById(R.id.help_image);
+            helpLeftArrow = findViewById(R.id.help_left_arrow);
+            helpRightArrow = findViewById(R.id.help_right_arrow);
+            helpPageNumber = findViewById(R.id.help_page_number);
+            helpExitButton = findViewById(R.id.help_exit_button);
+
+            //when right arrow is clicked it changes the page
+            helpRightArrow.setOnClickListener(v12 -> {
+                if (helpPageNumber.getText().toString().equals("Pg. 1")) {
+                    helpHeader1.setText(R.string.help_screen_header_1_page_2);
+                    helpText1.setText(R.string.help_screen_text_1_page_2);
+                    helpHeader2.setText(R.string.help_screen_header_2_page_2);
+                    helpText2.setText(R.string.help_screen_text_2_page_2);
+                    helpPageNumber.setText("Pg. 2");
+                    helpText2.setVisibility(VISIBLE);
+                    helpHeader2.setVisibility(VISIBLE);
+                    helpImage.setVisibility(GONE);
+                    helpRightArrow.setVisibility(VISIBLE);
+                    helpLeftArrow.setVisibility(VISIBLE);
+                } else if (helpPageNumber.getText().toString().equals("Pg. 2")) {
+                    helpHeader1.setText(R.string.help_screen_header_1_page_3);
+                    helpText1.setText(R.string.help_screen_text_1_page_3);
+                    helpHeader2.setText(R.string.help_screen_header_2_page_3);
+                    helpText2.setText(R.string.help_screen_text_2_page_3);
+                    helpPageNumber.setText("Pg. 3");
+                    helpText2.setVisibility(VISIBLE);
+                    helpHeader2.setVisibility(VISIBLE);
+                    helpImage.setVisibility(GONE);
+                    helpRightArrow.setVisibility(INVISIBLE);
+                    helpLeftArrow.setVisibility(VISIBLE);
+                }
+            });
+
+            //when left arrow is clicked it changes the page
+            helpLeftArrow.setOnClickListener(v13 -> {
+                if (helpPageNumber.getText().toString().equals("Pg. 2")) {
+                    helpHeader1.setText(R.string.help_screen_header_1_page_1);
+                    helpText1.setText(R.string.help_screen_text_1_page_1);
+                    helpPageNumber.setText("Pg. 1");
+                    helpText2.setVisibility(GONE);
+                    helpHeader2.setVisibility(GONE);
+                    helpImage.setVisibility(VISIBLE);
+                    helpRightArrow.setVisibility(VISIBLE);
+                    helpLeftArrow.setVisibility(INVISIBLE);
+                } else if (helpPageNumber.getText().toString().equals("Pg. 3")) {
+                    helpHeader1.setText(R.string.help_screen_header_1_page_2);
+                    helpText1.setText(R.string.help_screen_text_1_page_2);
+                    helpHeader2.setText(R.string.help_screen_header_2_page_2);
+                    helpText2.setText(R.string.help_screen_text_2_page_2);
+                    helpPageNumber.setText("Pg. 2");
+                    helpText2.setVisibility(VISIBLE);
+                    helpHeader2.setVisibility(VISIBLE);
+                    helpImage.setVisibility(GONE);
+                    helpRightArrow.setVisibility(VISIBLE);
+                    helpLeftArrow.setVisibility(VISIBLE);
+                }
+            });
         });
     }
 
