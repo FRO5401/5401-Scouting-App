2505d52bb50cff99fb83849223832365d02b50cd
Sophia <111466302+MintyLuve@users.noreply.github.com>
2026-02-20 19:42:33 -0500

    Updates, removed fuel passed from auto/tele

--- app/src/main/java/com/fro/scoutingapp/FragmentAuto.java
+++ app/src/main/java/com/fro/scoutingapp/FragmentAuto.java
@@ -13,8 +13,7 @@ public class FragmentAuto extends Fragment {
 
     // Declare variables
     RowLargeBox row1;
-    RowLargeBox row2;
-    RowTwoBoxes row3;
+    RowTwoBoxes row2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,17 +21,13 @@ public class FragmentAuto extends Fragment {
         // Init variables
         row1 = rootView.findViewById(R.id.auto_row_1);
         row2 = rootView.findViewById(R.id.auto_row_2);
-        row3 = rootView.findViewById(R.id.auto_row_3);
 
         // Row 1
         row1.createMultiCounter("Auto_Fuel_Scored", 1000, Values.colorType_greyWhiteGrey, 1,5);
 
         // Row 2
-        row2.createMultiCounter("Auto_Fuel_Passed", 1000, Values.colorType_greyWhiteGrey, 1, 5);
-
-        // Row 3
-        row3.createToggle("Auto_Climbed_L1", Values.left);
-        row3.createToggle("Auto_Used_Depot", Values.right);
+        row2.createToggle("Auto_Climbed_L1", Values.left);
+        row2.createToggle("Auto_Used_Depot", Values.right);
 
         // Inflate the layout for this fragment
         return rootView;
--- app/src/main/java/com/fro/scoutingapp/FragmentTeleop.java
+++ app/src/main/java/com/fro/scoutingapp/FragmentTeleop.java
@@ -14,7 +14,6 @@ public class FragmentTeleop extends Fragment {
     // Declare variables
     RowLargeBox row1;
     RowLargeBox row2;
-    RowLargeBox row3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,16 +21,12 @@ public class FragmentTeleop extends Fragment {
         // Init variables
         row1 = rootView.findViewById(R.id.teleop_row_1);
         row2 = rootView.findViewById(R.id.teleop_row_2);
-        row3 = rootView.findViewById(R.id.teleop_row_3);
 
         // Row 1
         row1.createMultiCounter("Teleop_Fuel_Scored", 1500, Values.colorType_greyWhiteGrey, 1, 5);
 
         // Row 2
-        row2.createMultiCounter("Teleop_Fuel_Passed", 1500, Values.colorType_greyWhiteGrey, 1, 5);
-
-        // Row 3
-        row3.createCounter("Teleop_Times_Fuel_Stored_Outpost", 50);
+        row2.createCounter("Teleop_Times_Fuel_Stored_Outpost", 50);
 
         // Inflate the layout for this fragment
         return rootView;
